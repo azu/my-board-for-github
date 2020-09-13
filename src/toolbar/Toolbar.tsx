b9f47ca526e55d9a39dc23d73bc78118d0aa367e
azu <azuciao@gmail.com>
2020-09-13 16:58:50 +0900

    feat(Toolbar): add icon

--- src/toolbar/Toolbar.tsx
+++ src/toolbar/Toolbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Toolbar.css";
-import { ArchiveIcon, DatabaseIcon } from "@primer/octicons-react";
+import { SyncIcon, RocketIcon, ArchiveIcon, DatabaseIcon, MarkGithubIcon } from "@primer/octicons-react";
 import { env } from "../env";
 
 export type ToolbarProps = {
@@ -12,29 +12,52 @@ export const Toolbar = (props: ToolbarProps) => {
     const dbURL = `https://github.com/${env.owner}/${env.repo}`;
     return (
         <div className={"Toolbar"}>
-            <button className={"ToolbarButton"} onClick={props.onRefresh} title={"Reload metadata and refresh items"}>
-                Refresh
-            </button>
-            <button
-                className={"ToolbarButton"}
-                onClick={props.onApplyAutoRule}
-                title={"Apply automation. Move closed issue to done column."}
-            >
-                Apply Auto Rule
-            </button>
-            <button className={"ToolbarButton"} onClick={props.onArchive} title={"Archive all cards that are in done"}>
-                <ArchiveIcon size={16} />
-                Archive
-            </button>
-            <a
-                className={"ToolbarButton"}
-                href={dbURL}
-                title={"See your database"}
-                target={"_blank"}
-                rel="noopener noreferrer"
-            >
-                <DatabaseIcon size={16} />
-            </a>
+            <div className={"ToolbarMenu"}>
+                <button
+                    className={"ToolbarButton"}
+                    onClick={props.onRefresh}
+                    title={"Reload metadata and refresh items"}
+                >
+                    <SyncIcon size={16} className={"ToolbarButtonIcon"} />
+                    Refresh
+                </button>
+                <button
+                    className={"ToolbarButton"}
+                    onClick={props.onApplyAutoRule}
+                    title={"Apply automation. Move closed issue to done column."}
+                >
+                    <RocketIcon size={16} className={"ToolbarButtonIcon"} />
+                    Format task
+                </button>
+                <button
+                    className={"ToolbarButton"}
+                    onClick={props.onArchive}
+                    title={"Archive all cards that are in done"}
+                >
+                    <ArchiveIcon size={16} className={"ToolbarButtonIcon"} />
+                    Archive
+                </button>
+                <a
+                    className={"ToolbarButton"}
+                    href={dbURL}
+                    title={"See your database"}
+                    target={"_blank"}
+                    rel="noopener noreferrer"
+                >
+                    <DatabaseIcon size={16} className={"ToolbarButtonIcon"} />
+                </a>
+            </div>
+            <div className={"ToolbarMetaMenu"}>
+                <a
+                    className={"ToolbarButton"}
+                    href={"https://github.com/azu/my-board-data"}
+                    title={"Source Code"}
+                    target={"_blank"}
+                    rel="noopener noreferrer"
+                >
+                    <MarkGithubIcon size={16} className={"ToolbarButtonIcon"} />
+                </a>
+            </div>
         </div>
     );
 };
